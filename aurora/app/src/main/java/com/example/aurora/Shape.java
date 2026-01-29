d8d63ba43da91493078e667ba793745e88c52c2e
sross334 <sross4@ualberta.ca>
2026-01-28 18:36:36 -0700

    fixed code

--- aurora/app/src/main/java/com/example/aurora/Shape.java
+++ aurora/app/src/main/java/com/example/aurora/Shape.java
@@ -3,11 +3,5 @@ package com.example.aurora;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
-
     private String color="blue";
-=======
-    
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> 66066cb6fd065cd794eb71767b588a4f142ea8c8
 }
